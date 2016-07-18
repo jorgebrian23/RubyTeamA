16d88640243e4917f6a434fa49b561c5ee750fe7
AlvaroDaza <Alvaro.Daza@fundacion-jala.org>
2016-07-18 17:58:17 -0400

    acceptance test get and one put

--- src/data/project.rb
+++ src/data/project.rb
@@ -1,6 +1,7 @@
 require_relative 'base_class_for_data_classes'
+require_relative '../../src/helpers/data_helper'
 
-class Project <BaseClassForDataClasses
+class Project < BaseClassForDataClasses
   attr_accessor :account_id,
                 :atom_enabled,
                 :automatic_planning,
@@ -32,6 +33,26 @@ class Project <BaseClassForDataClasses
                 :week_start_day
   def initialize (values = {})
     super values
+  end
+
+  def self.get_array(string)
+    array_projects = []
+    object_json = DataHelper.get_json(string)
+    object_json.each {| value |
+      array = DataHelper.rehash_to_symbol_keys(value)
+      auxiliar = Project.new(array)
+      array_projects.push(auxiliar)
+    }
+    array_projects
+  end
+
+  def self.get_project(string)
+    object_json =DataHelper.get_json(string)
+    puts "hasta aca llegue!"
+    result=DataHelper.rehash_to_symbol_keys(object_json)
+    project=Project.new(result)
+
+    return project
   end
 
 end
